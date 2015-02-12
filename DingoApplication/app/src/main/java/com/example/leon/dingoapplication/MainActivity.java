5195646b796e72140f8f6700c84ea71bcf4479cf
Leon <lee_quee_leong82@hotmail.com>
2015-02-12 23:17:25 +0800

    Overall Layout of the Customer View is completed. Creation of deals list view under tab "All" (Continues)

--- DingoApplication/app/src/main/java/com/example/leon/dingoapplication/MainActivity.java
+++ DingoApplication/app/src/main/java/com/example/leon/dingoapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.leon.dingoapplication;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -9,7 +10,6 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,6 +31,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
      */
     ViewPager mViewPager;
 
+    /*
+    Context variable to store resources
+     */
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +43,14 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         setContentView(R.layout.activity_main);
 
         // Create the adapter that will return a fragment for each of the three primary sections
-        mAppSectionPagerAdapter = new AppSectionPagerAdapter(getSupportFragmentManager());
+        mAppSectionPagerAdapter = new AppSectionPagerAdapter(getSupportFragmentManager(), getApplicationContext());
 
         // Set up the action bar
         final ActionBar actionBar = getActionBar();
 
+        // Customized title as TextView in the ActionBar
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.actionbar_custom);
 
         // Specify that the home/up button should not be enables since there is no hierarchical parent
         actionBar.setHomeButtonEnabled(false);
@@ -52,7 +60,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
         //Set up the viewpager, attaching the adapter and setting up a listener for when the user
         // swipe between the sections
-        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager = (ViewPager) findViewById(R.id.customer_home_pager);
         mViewPager.setAdapter(mAppSectionPagerAdapter);
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
@@ -71,17 +79,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                             .setTabListener(this));
         }
 
-        /*
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-        */
     }
 
     @Override
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // Actions when tab is being unselected
     }
 
     @Override
@@ -92,6 +94,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // Actions when the tab is reselected
     }
 
     /*
@@ -99,16 +102,21 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     sections of the app.
      */
     public static class AppSectionPagerAdapter extends FragmentStatePagerAdapter {
-        public AppSectionPagerAdapter(FragmentManager fm) {
+        Context mContext;
+        public AppSectionPagerAdapter(FragmentManager fm, Context context) {
             super(fm);
+            mContext = context;
         }
 
         @Override
         public Fragment getItem(int i) {
            switch(i) {
                case 0:
-                   return new LaunchpadSectionFragment();
-
+                   return new CustomerViewAll();
+               case 1:
+                   return new CustomerViewDings();
+               case 2:
+                   return new CustomerViewMap();
                default:
                    Fragment fragment = new PlaceholderFragment();
                    Bundle args = new Bundle();
@@ -125,10 +133,45 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return "Section " + (position + 1);
+            // Dynamic return of title name according to the tab number from string.xml
+            String tabString = "tab" + (position + 1) + "_text";
+            int resID = mContext.getResources()
+                    .getIdentifier(tabString, "string", mContext.getPackageName());
+            return mContext.getResources().getString(resID);
         }
     }
 
+    // Fragment that display all deals available for customers
+    public static class CustomerViewAll extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.customer_view_all, container, false);
+
+            return rootView;
+        }
+    }
+
+    public static class CustomerViewDings extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.customer_view_dings, container, false);
+
+            return rootView;
+        }
+    }
+
+    public static class CustomerViewMap extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.customer_view_map, container, false);
+
+            return rootView;
+        }
+    }
+    /*
     // A fragment that launch other parts of the application
     public static class LaunchpadSectionFragment extends Fragment {
         @Override
@@ -152,12 +195,12 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                     .setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            /*
+                            *//*
                             Create an intent that ask user to pick a photo, but using
                             FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET, ensures that relaunching
                             the application from the device home screen does not return
                             to the external activity
-                             */
+                            *//*
                             Intent externalActivityIntent = new Intent(Intent.ACTION_PICK);
                             externalActivityIntent.setType("image/*");
                             externalActivityIntent.addFlags(
@@ -170,9 +213,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
             return rootView;
         }
     }
+    */
 
     /**
-     * A placeholder fragment containing a simple view.
+     * A placeholder fragment containing default view in a section.
+     * To be removed
      */
     public static class PlaceholderFragment extends Fragment {
 
